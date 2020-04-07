984bbb55096803b13151bdb76bb821ee633d8b66
Todd Peterson <ToddPetersonGV@gmail.com>
2020-04-07 14:31:22 -0600

    Add template for deliverable 4

---
+++ deliverable4.sql
@@ -0,0 +1,57 @@
+-- 1. Show the following employee information: first name, last name, EmployeeID. They should be listed alphabetically by last name first, if more than one employee has the same last name them sort them alphabetically byfirst name. 
+
+
+
+-- 2. Show the following customer information: first name, last name, telephone number, address, city, state, zipcode; they should be listed alphabetically by last name first. If more than one customerhas the same last name,thensort them alphabetically be first name.
+
+
+
+-- 3. Given a customer name, list all of the times that thecustomervisited the restaurant. Include how the customer paid and at which table they sat.Do not include delivery or carry out orders.Pick a name from your dataset, but the name should be able to easily be replaced.
+
+
+
+-- 4. List the Employeenumber,employee job title,hours worked, salary per hour, and total weekly salary for each technician who worked more than 40 hoursfor a given week.You will need to pick a start and end date, but these should be able to be changed easily.
+
+
+
+-- 5. List the menu item name anddescription for all products which contain an ingredientthat has anamewhichstarts with a "B" and has a third letter of "k".  (Your answer should include Baking Powder, Baking Soda, and Baker’s Chocolate.)
+
+
+
+-- 6. List all vendorsin alphabetical orderand each ingredientthey supply and the first day that item was ever delivered and the last day that ingredient was ever delivered from that vendor.
+
+
+
+-- 7. List the customer name, table number,entrée ordered,and dateand time orderedfor all items purchased on a given date.(e.g. May 5th2010)
+
+
+
+-- 8. List food items(customer number, customer name, item ordered,item type,price,whether dine in, carry out, or delivery) for all orders associated with a specified customer. The query should a given customernumber or name.Pick a nameand numberfrom your dataset, but the name should be able to easily be replaced.
+
+
+
+-- 9. Products
+--      a. For eachcuisinelist the menu item, description, and the total number of sales from that cuisine.
+--      b. As a continuation from a, list the menu item, description, and the total number of sales for the cuisinethat has the highest number of sales.
+
+
+
+-- 10. List the menu item, description, and number of suppliers for those ingredientsthat can be supplied from multiple suppliers.
+
+
+
+-- 11. List the order number, total price, and count of ticket items for any order where only water was ordered.Only includetickets whereevery drinkon the ticket ordered was water (free drinks).
+
+
+
+-- 12. List the order number, total price, and count of ticket items for any order where any drink other than water was ordered.If water was also ordered, it will still be included as long as anyone else on the ticket ordered somethingother than water (free drinks).
+
+
+
+-- 13. List any menu item that has never been ordered.
+
+
+
+-- 14. List any cuisine that has never had an item ordered from it.
+
+
