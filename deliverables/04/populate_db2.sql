eab849800d66e2ec3178643000955b15fd40699c
Todd Peterson <ToddPetersonGV@gmail.com>
2020-04-16 10:02:23 -0600

    Add Ingredients and MenuItems matching 'B_K%' for query 5

--- deliverables/04/populate_db2.sql
+++ deliverables/04/populate_db2.sql
@@ -1,7 +1,20 @@
 --
 -- Add data related to deliverable 4
 --
+USE BobsAwesomeEatery
+GO
 
+-- For query 5
+
+INSERT INTO MenuItems VALUES
+	('Cookies', 'You cook them. You eat them. Such is life', 'C:\picture\cookie.jpg', 0, 40, 1, 3, 3)
+
+DECLARE @CookieID int = @@IDENTITY
+
+INSERT INTO Ingredients VALUES
+	('Baking Soda', 'Pantry', @CookieID, 2),
+	('Baking Powder', 'Pantry', @CookieID, 2),
+	('Baker''s Chocolate', 'Pantry', @CookieID, 4)
 
 -- For query 14
 INSERT INTO CuisineTypes VALUES ('Swedish')
