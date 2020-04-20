b7fe9e2a75e8fa00b509986feeb193fb41f5ccca
Todd Peterson <ToddPetersonGV@gmail.com>
2020-04-20 17:48:07 -0600

    Add VS solution for final deliverable

---
+++ src/EateryLibrary/GlobalConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EateryLibrary
+{
+    public static class GlobalConfig
+    {
+        /// <summary>
+        /// Returns the requested connection string
+        /// </summary>
+        /// <param name="name">The name of a connection (ie. "BobsAwesomeEatery")</param>
+        /// <returns></returns>
+        public static string ConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
---
+++ src/EateryLibrary/Models/MenuItem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EateryLibrary.Models
+{
+    public class MenuItem
+    {
+		#region Private Variables
+		private int _ID;
+		private string _Name;
+		private string _Description;
+		private string _PicturePath;
+		private bool _IsSideItem;
+		private int _PrepTime;
+		#endregion
+
+		#region Public Variables
+		public int ID
+		{
+			get { return _ID; }
+			set { _ID = value; }
+		}
+
+		public string Name
+		{
+			get { return _Name; }
+			set { _Name = value; }
+		}
+
+		public string Description
+		{
+			get { return _Description; }
+			set { _Description = value; }
+		}
+
+		public string PicturePath
+		{
+			get { return _PicturePath; }
+			set { _PicturePath = value; }
+		}
+
+		public bool IsSideItem
+		{
+			get { return _IsSideItem; }
+			set { _IsSideItem = value; }
+		}
+
+		public int PrepTime
+		{
+			get { return _PrepTime; }
+			set { _PrepTime = value; }
+		}
+		#endregion
+
+
+	}
+}
