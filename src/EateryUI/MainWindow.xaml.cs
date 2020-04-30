ae85af60f0031917e22e61d8d6ab1a907f88b3b3
Todd Peterson <ToddPetersonGV@gmail.com>
2020-04-30 17:51:38 -0600

    Resolve build error

--- src/EateryUI/MainWindow.xaml.cs
+++ src/EateryUI/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-//using System.Windows.Controls;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -14,8 +14,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using EateryLibrary;
-using EateryLibrary.Models;
 
 
 namespace EateryUI
